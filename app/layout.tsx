d8e2092c838601dcecc0a2956d07dbfc08171c73
hikmet <aydin96tr@gmail.com>
2023-10-31 17:21:38 +0300

    add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
+import { rubik } from "@/app/ui/fonts";
 import "@/app/ui/global.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${rubik.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Lusitana, Rubik } from "next/font/google";
+
+export const rubik = Rubik({ weight: "400", subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
